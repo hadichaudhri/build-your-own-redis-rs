90b5df7cd8f8cb26ba4e1856aa440427c60f9f79
Hadi Chaudhri <hadichaudhri@gmail.com>
2023-03-23 12:53:47 -0400

    Respond to PING command

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        if let Err(e) = stream {
+            println!("error: {}", e);
+            return Err(e);
         }
+        stream?.write(b"+PONG\r\n")?;
     }
+    Ok(())
 }
